db2ee3124e9e7ecbb0d7154452490aff97b542b2
jitender.singh <jitender.singh@salesforce.com>
2021-12-24 16:43:12 +0530

    use previously decrypted files if any

--- app/src/main/java/jp/id/core/Decrypter.java
+++ app/src/main/java/jp/id/core/Decrypter.java
@@ -18,9 +18,13 @@ import javax.crypto.spec.SecretKeySpec;
 public class Decrypter {
 
     public static File decrypt(byte[] key, File file, String dir) {
-        File outFilePath;
+        File outFilePath = new File(String.format("%s/%s.ts", dir, file.getName()));
 
-         outFilePath = new File(String.format("%s/%s.ts", dir, file.getName()));
+        // if we have a decrypted file already (from previous download), skip.
+        if (outFilePath.exists()) {
+            return outFilePath;
+        }
+
         byte[] iv = "\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000".getBytes(StandardCharsets.UTF_8);
 
         try {
