fb621bfe2885fbb803c9fc01f82ffb29c578b984
jitender.singh <jitender.singh@salesforce.com>
2022-01-08 18:10:35 +0530

    handle download progress in-memory, restore last scroll position

--- app/src/main/java/jp/id/model/Lectures.java
+++ app/src/main/java/jp/id/model/Lectures.java
@@ -3,8 +3,20 @@ package jp.id.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jp.id.core.Utils;
+
 public class Lectures {
     private static final List<LectureItem> lectureItems = new ArrayList<>();
+    private static int numDownloadsInProgress = 0;
+    private static int lastPosition = 0;
+
+    public static int getLastPosition() {
+        return lastPosition;
+    }
+
+    public static void setLastPosition(int lastPosition) {
+        Lectures.lastPosition = lastPosition;
+    }
 
     private Lectures() {}
 
@@ -15,5 +27,18 @@ public class Lectures {
     public static void setLectures(List<LectureItem> items) {
         lectureItems.clear();
         lectureItems.addAll(items);
+    }
+
+    public static void incrementDownloads() {
+        numDownloadsInProgress += 1;
+    }
+
+    public static void decrementDownloads() {
+        numDownloadsInProgress -= 1;
+        numDownloadsInProgress = Math.max(0, numDownloadsInProgress);
+    }
+
+    public static boolean isDownloadInProgress() {
+        return numDownloadsInProgress > 0;
     }
 }
